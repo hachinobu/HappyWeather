a9547874895e7cbe567eedb5207308bd7cfdeb78
nishinobu <nishinobu@atlas.jp>
2014-06-30 16:19:31 +0900

    ライブラリやリソース追加

---
+++ HappyWeather/UIColor+HexColor.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+HexColor.h
+//  HappyWeather
+//
+//  Created by Takahiro Nishinobu on 2014/06/30.
+//  Copyright (c) 2014年 Takahiro Nishinobu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (HexColor)
+
+@end
---
+++ HappyWeather/UIColor+HexColor.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+HexColor.m
+//  HappyWeather
+//
+//  Created by Takahiro Nishinobu on 2014/06/30.
+//  Copyright (c) 2014年 Takahiro Nishinobu. All rights reserved.
+//
+
+#import "UIColor+HexColor.h"
+
+@implementation UIColor (HexColor)
+
+@end
